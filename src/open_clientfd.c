620a0c062ab20431cef9cc09a341ef5a87fdc34c
ylwdream <ylw@mail.ustc.edu.cn>
2015-03-17 07:43:47 -0700

    new

---
+++ src/open_clientfd.c
@@ -0,0 +1,24 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  open_clientfd.c
+ *
+ *    Description: 包装客户端的创建socket和connect操作
+ *
+ *        Version:  1.0
+ *        Created:  03/17/2015 07:04:24 AM
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  WYL (), ylwzzu@gmail.com
+ *
+ * =====================================================================================
+ */
+
+#include "tiny.h"
+
+int open_clientfd(char *hostname, int port)
+{
+    int clientfd;
+	    
+}
